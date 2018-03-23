1c094183cf8da8ac894040f6a4a333fb0dc403a9
Claudia M. A <claumartal@gmail.com>
2018-03-23 17:56:51 +0100

    Add files via upload

--- temperatura.ino
+++ temperatura.ino
@@ -1,42 +1,31 @@
-// Incluimos librería
 #include <DHT.h>
  
-// Definimos el pin digital donde se conecta el sensor
+
 #define DHTPIN 2
-// Dependiendo del tipo de sensor
 #define DHTTYPE DHT11
 int i = 0;
-// Inicializamos el sensor DHT11
+
 DHT dht(DHTPIN, DHTTYPE);
  
 void setup() {
-  // Inicializamos comunicación serie
   Serial.begin(9600);
-  // Comenzamos el sensor DHT
   dht.begin();
  
 }
  
 void loop() {
-    // Esperamos 5 segundos entre medidas
-  delay(5000);
+  delay(3000); // 3 segundos
  
-  // Leemos la humedad relativa
   float h = dht.readHumidity();
-  // Leemos la temperatura en grados centígrados (por defecto)
   float t = dht.readTemperature();
-  // Leemos la temperatura en grados Fahreheit
   float f = dht.readTemperature(true);
  
-  // Comprobamos si ha habido algún error en la lectura
   if (isnan(h) || isnan(t) || isnan(f)) {
-    Serial.println("Error obteniendo los datos del sensor DHT11");
+    Serial.println("DHT Sensor error");
     return;
   }
  
-  // Calcular el índice de calor en Fahreheit
   float hif = dht.computeHeatIndex(f, h);
-  // Calcular el índice de calor en grados centígrados
   float hic = dht.computeHeatIndex(t, h, false);
   if(i != 0) {
     Serial.print("Humedad: ");
