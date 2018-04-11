a27443d2548e9e57daf764ace43b7ab533380350
Claudia M. A <claumartal@gmail.com>
2018-04-11 11:54:57 +0200

    Update sendDataToRaspberry.ino

--- sendDataToRaspberry.ino
+++ sendDataToRaspberry.ino
@@ -1,1 +1,13 @@
 
+import serial
+
+s = serial.Serial('/dev/ttyUSB0',9600)
+serialData = []
+
+while True:
+    data = s.readline()   
+    if data:                
+        serialData.append(data)
+    if len(serialData) == 4:        
+        print s             
+        s = []              
